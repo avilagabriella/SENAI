b6624daa4507819abe8be4496ee16968f20fe7db
avilagabriella <avilagabriella84@gmail.com>
2024-04-16 10:40:59 -0300

    aulas

---
+++ 2DES/PWBE/Aula 06/bd/script.sql
@@ -0,0 +1,22 @@
+drop database if exists SGT;
+create database SGT CHARSET=UTF8 COLLATE utf8_general_ci;
+use SGT;
+
+create table Usuarios(
+    idUsuarios int not null primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(50) not null,
+    senha varchar(10) not null
+);
+create table Tarefas(
+    idTarefas int not null primary key auto_increment,
+    descricao varchar(100) not null default("Tarefas"),
+    DataVencimento varchar(30) not null,
+    status varchar(20) not null default
+);
+
+
+describe Usuarios;
+describe Tarefas;
+show tables;
+
