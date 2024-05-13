370b59da47ae7fc01b46adc0324790d40d810aaa
avilagabriella <avilagabriella84@gmail.com>
2024-05-13 11:24:05 -0300

    aulas

---
+++ 2DES/PWFE/Aula 13/Nosso Quiz/questions.js
@@ -0,0 +1,104 @@
+export default [
+    {
+        question: "Qual palavra está escrita corretamente?",
+        answers: [
+            {option: "Resiliencia", correct:true},
+            {option: "Resiliência", correct:false},
+            {option: "Resiliênsia", correct:false},
+            {option: "Resiliênsia", correct:false},
+        ],
+    },
+
+    {
+        question: "Qual das alternativas abaixo faz parte do grupo: Miúdo, Saída, Piauí e Araújo",
+        answers: [
+            {option: "Cajá", correct:false},
+            {option: "Bibelô", correct:false},
+            {option: "Saúva", correct:true},
+            {option: "Pólen", correct:false},
+        ],
+    },
+
+    {
+        question: "O que este símbolo '!' representa na língua portuguesa?",
+        answers: [
+            {option: "Afirmação", correct:false},
+            {option: "Exclamação", correct:true},
+            {option: "Interrupção", correct:false},
+            {option: "Interrogação", correct:false},
+        ],
+    },
+
+    {
+        question: "Quanto é 7 Qual das alternativas devemos acrescentar a 'crase'?",
+        answers: [
+            {option: "Meu restaurante vende bife a cavalo", correct:true},
+            {option: "A menina vai a praia", correct:false},
+            {option: "Foi a Roma,voltei de Roma", correct:false},
+            {option: "Pediu para a chefe autorização para acampar", correct:false},
+        ],
+    },
+
+    
+    {
+        question: "Qual das alternativas abaixo está escrito corretamente?",
+        answers: [
+            {option: "auto-escola", correct:false},
+            {option: "auto-retrato", correct:false},
+            {option: "infra-estrutura", correct:false},
+            {option: "anti-inflamatório", correct:true},
+        ],
+    },
+
+    
+    {
+        question: "Qual das alternativas abaixo o 's' não se encaixa",
+        answers: [
+            {option: "Solo", correct:false},
+            {option: "Sete", correct:false},
+            {option: "Sapato", correct:false},
+            {option: "Crensa", correct:true},
+        ],
+    },
+
+    
+    {
+        question: "Qual palavra está escrita incorretamente?",
+        answers: [
+            {option: "Benção", correct:true},
+            {option: "Pássaro", correct:false},
+            {option: "Hífen", correct:false},
+            {option: "Início", correct:false},
+        ],
+    },
+
+    
+    {
+        question: "Marque a alternativa correta do substantivo composto 'quinta-feira'",
+        answers: [
+            {option: "Quinta-feiras", correct:false},
+            {option: "Quintas-feiras", correct:true},
+          
+        ],
+    },
+
+    
+    {
+        question: "Marque a alternativa correta do plural dos adjetivos compostos 'luso-brasileiro' ",
+        answers: [
+            {option: "Lusos-brasileiros", correct:false},
+            {option: "Luso-brasileiros", correct:true},
+            
+        ],
+    },
+
+    
+    {
+        question: "Marque a alternativa correta do plural dos adjetivos compostos 'Verde-claro' ",
+        answers: [
+            {option: "Verdes-claros", correct:false},
+            {option: "Verde-claros", correct:true},
+            
+        ],
+    },
+];
